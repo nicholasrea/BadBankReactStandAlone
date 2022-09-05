a95c2ad8403733231c73f2d1e60bd2641111b3e3
nicholasrea <n.rea08@gmail.com>
2022-09-05 11:32:25 -0400

    Updated

--- withdraw.js
+++ withdraw.js
@@ -2,13 +2,6 @@ function Withdraw() {
   const ctx = React.useContext(UserContext);
   
   return(
-    <Form
-      type='input'
-      name='name'
-      placeholder='name'
-      
-      />
-    // Commented out for testing, Uncomment for functionality
-    // <Transaction id="Withdraw" />
+    <Transaction id="Withdraw" />
   );
 }
